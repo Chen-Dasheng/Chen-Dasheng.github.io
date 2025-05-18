d1bcd5514aa5ba1df492b049c744891745963d4d
SnapLittlebear <2135279336@qq.com>
2025-05-18 17:18:00 +0800

    modified:   createIframe.js

--- createIframe.js
+++ createIframe.js
@@ -3,8 +3,20 @@ function createIframe() {
     iFrame.id = "iframe";
     iFrame.title = "iframe";
     iFrame.width = "100%";
-    iFrame.height = document.getElementById("iframeHeight").value;
-    iFrame.src = document.getElementById("targetURL").value;
-    document.body.appendChild(iFrame);
+    var height = document.getElementById("iframeHeight").value;
+    var src = document.getElementById("targetURL").value;
+    if (height == "") {
+        iFrame.height = "800"; 
+    }
+    else {
+        iFrame.height = height; 
+    }
+    if (src == "") {
+        iFrame.src = "/webInsert.html" 
+    }
+    else {
+        iFrame.src = src; 
+    }
+    window.parent.document.body.appendChild(iFrame);
     return 'success';
 }
