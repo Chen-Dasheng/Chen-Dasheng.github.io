a709ddb6706ce10c7479b398cc11015d06edfe4f
SnapLittlebear <2135279336@qq.com>
2025-05-18 13:39:48 +0800

    Small progress

--- createIframe.js
+++ createIframe.js
@@ -1,10 +1,10 @@
-function CreateIframe(targetURL) {
+function createIframe() {
     var iFrame = document.createElement("iframe");
     iFrame.id = "iframe";
     iFrame.title = "iframe";
     iFrame.width = "100%";
-    iFrame.height = "500";
-    iFrame.src = targetURL;
+    iFrame.height = document.getElementById("iframeHeight").value;
+    iFrame.src = document.getElementById("targetURL").value;
     document.body.appendChild(iFrame);
     return 'success';
 }
