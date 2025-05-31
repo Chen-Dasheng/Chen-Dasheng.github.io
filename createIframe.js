1aa82735286555d767fee97f479c99ba07b091a5
SnapLittlebear <2135279336@qq.com>
2025-05-31 16:31:56 +0800

    modified:   createIframe.js modified:   webInsert.html

--- createIframe.js
+++ createIframe.js
@@ -1,32 +1,22 @@
-function createIframe(mode=0, targetURL, height) {
-    var height = height || document.getElementById("iframeHeight").value;
-    var src = targetURL || document.getElementById("targetURL").value;
-    if (src == "") {
-        var object = document.createElement("object");
-        object.width = "100%";
-        object.data = "/webInsert.html";
-        window.document.body.appendChild(object);
-        return'success';
+function createIframe(targetURL, height) {
+    var iframe = document.createElement("iframe");
+    iframe.id = "iframe";
+    iframe.title = "iframe";
+    iframe.width = "100%";
+    if (targetURL === undefined) {
+        targetURL = "snaplittlebear.top/index.html";
     }
-    else {
-        var iframe = document.createElement("iframe");
-        iframe.id = "iframe";
-        iframe.title = "iframe";
-        iframe.width = "100%";
-        iframe.frameBorder = "0";
-        iframe.src = src; 
-        if (height == "") {
-            iframe.height = "800"; 
-        }
-        else {
-            iframe.height = height; 
-        }
-        if (mode == 0){
-            window.document.body.appendChild(iframe);
-        }
-        else{
-            window.parent.document.body.appendChild(iframe);
-        }
-        return 'success';
+    if (height === undefined) {
+       height = "650px"; 
     }
+    iframe.frameBorder = "0";
+    iframe.src = src; 
+    iframe.height = height; 
+    iframe.sandbox = "allow-top-navigation allow-same-origin allow-forms allow-scripts";
+    if (!targetURL.includes("://")) {
+        targetURL = "https://" + targetURL;
+    }
+    iframe.allowfullscreen = true;
+    window.document.body.appendChild(iframe);
+    return 'success';
 }
